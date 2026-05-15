c495d1d6671bd11a54f389984794da374fe739b2
unknown <bimasakti.kr@gmail.com>
2026-05-15 12:53:10 +0700

    New Link - time gate - workspace

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main style={{ width: '100vw', height: '100vh', margin: 0, padding: 0, overflow: 'hidden', backgroundColor: '#f4f5f7' }}>
       <iframe
         // ⚠️ SANGAT PENTING: Ganti URL di bawah dengan link Web App Google Script Anda
-        src="https://script.google.com/macros/s/AKfycbzgL2KYmb3J73y1Cr1BjFDIP5s4zxSc1jZklSj7KWpYgVRycC-yIehDvrRGc_M6Pg77/exec"
+        src="https://script.google.com/macros/s/AKfycbxUuO4FR26MFOqya5ji-w9CWwWOenDmb6OhvrPFF1iHMj8GYS-VYdV1U9QHEmEP1QQQ/exec"
         style={{
           width: '100%',
           height: '100%',
