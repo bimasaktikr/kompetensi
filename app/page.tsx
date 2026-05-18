f490962f3679ee12c1db6a234e8be219e0a055a3
unknown <bimasakti.kr@gmail.com>
2026-05-18 08:49:29 +0700

    dev 3.0.8

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ export default function Home() {
     <main style={{ width: '100vw', height: '100vh', margin: 0, padding: 0, overflow: 'hidden', backgroundColor: '#f4f5f7' }}>
       <iframe
         // ⚠️ SANGAT PENTING: Ganti URL di bawah dengan link Web App Google Script Anda
-        src="https://script.google.com/macros/s/AKfycbwpsAmP21kcUM1yRxACtyWp2BpXqNyZZggJ6xrc3Sg8PoUck_k7W8eYzR88472xDT9z/exec"
+        src="https://script.google.com/macros/s/AKfycbzOOy05qDlCL0EXevIqe4nbqvYQMpVWQEPAVT42qtr6jY9yznVuUZ6sJg8W0msCLSwp/exec"
         style={{
           width: '100%',
           height: '100%',
           border: 'none',
           display: 'block'
         }}
-        title="Command Center SE2026"
+        title="Command Center SE2026" 
         allowFullScreen
       />
     </main>
