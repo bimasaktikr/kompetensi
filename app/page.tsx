07dae9c61715c5d2c1a2892833c68ba1be9c0a5f
bimasaktikr <bimasakti.kr@gmail.com>
2026-05-25 13:28:11 +0700

    add

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main style={{ width: '100vw', height: '100vh', margin: 0, padding: 0, overflow: 'hidden', backgroundColor: '#f4f5f7' }}>
       <iframe
         // ⚠️ SANGAT PENTING: Ganti URL di bawah dengan link Web App Google Script Anda
-        src="https://script.google.com/macros/s/AKfycbwR89mcUfFQY2Pf_qekYb0znE74X2nIUMeyQP3HK3Lz36VooS6obrtVTRG_sRZ-sjzw/exec"
+        src="https://script.google.com/macros/s/AKfycbyLlkGxrzyobQve88jbMSzZ5aeaIFGvHV2bIKCA2Bg1Pew-mk4qSqlevZDfVCXhjobA/exec"
         style={{
           width: '100%',
           height: '100%',
