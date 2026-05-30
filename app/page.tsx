320f069bf535c300cde47f0bc2e910228c0187c3
unknown <bimasakti.kr@gmail.com>
2026-05-30 07:20:16 +0700

    Affirmasi 2

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main style={{ width: '100vw', height: '100vh', margin: 0, padding: 0, overflow: 'hidden', backgroundColor: '#f4f5f7' }}>
       <iframe
         // ⚠️ SANGAT PENTING: Ganti URL di bawah dengan link Web App Google Script Anda
-        src="https://script.google.com/macros/s/AKfycbzWgb5VAR70GxKoYuAE8w-KLPpvbgWvdENguOAvVHzqVH2t_dNf_NJHE3JyAe8yxchV/exec"
+        src="https://script.google.com/macros/s/AKfycby_7xFE1nCtUPAKRbCebPmwDsBMuxOxLTJUJn66b929SMl15FTWU4Cfmj5WuAvbhSjq/exec"
         style={{
           width: '100%',
           height: '100%',
